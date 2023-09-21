f5b92f6a084e1de88573b9dd53e9948d6e0f2c01
gloirebeniche <gloirebeniche11@gmail.com>
2023-09-21 08:09:25 +0100

    ajouter des collections et protection contre les attaques par injections XSS

--- tasks/models.py
+++ tasks/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Collection(models.Model):
     name = models.CharField(max_length=60)
     slug = models.SlugField()
-    
+
 
 class Task(models.Model):
+    # on_delete = models.CASCADE permet supprimer les elements associé à l'attribut pere vu que c'est une clé
+    # étrangères
     description = models.CharField(max_length=300)
-    
-    # on_delete = models.CASCADE permet supprimer les elements associé à l'attribut pere vu que c'est une clé  secondaire
     Collection = models.ForeignKey(Collection, on_delete=models.CASCADE)
--- tasks/views.py
+++ tasks/views.py
@@ -1,11 +1,29 @@
+from html import escape
+
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from tasks.models import Collection
+
 
 # Create your views here.
-  
+
 def index(request):
-    return render(request, 'tasks/index.html', context={})
+    context = {"collections": Collection.objects.order_by("name")}
+    # récuperation de toutes les collections créer on peut mettre all().
+    # qui sera passer via le context
+    return render(request, 'tasks/index.html', context=context)
+
 
 def add_collection(request):
-    return HttpResponse("")
- 
+    # récupération de la collection depuis le formulaire
+    # protection contre les attaques par injection XSS  en utilisant escape()
+    collection_name = escape(request.POST.get("collection_name"))
+    # création de la collection
+    # verification des si l'object à été creer avec la methode get_or_create(object, created)
+    collection,created = Collection.objects.get_or_create(name=collection_name)
+
+    if not created:
+        return HttpResponse("la collection existe déjà")
+
+    return redirect("home")
